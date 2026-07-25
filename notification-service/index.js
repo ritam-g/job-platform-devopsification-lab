d0cd8798866ccb5d3f42306d1f0110f0786848b7
ritam-g <ritammaty2006@gmail.com>
2026-07-25 22:52:14 +0530

    feat(notification-service): remove socket.io integration and update dependencies

--- notification-service/index.js
+++ notification-service/index.js
@@ -1,25 +1,12 @@
 import "./src/config/env.js"
 
-import http from "http"
 import app from "./app.js"
 import connectDb from "./src/db/index.js"
-import { Server } from "socket.io"
-import { notificationSocketHandler } from "./src/sockets/notify.js"
-import { setIO } from "./src/sockets/socketInstance.js"
 import { consumeNotifications } from "./src/queues/notificationConsumer.js"
-
-const server = http.createServer(app)
-const io = new Server(server, {
-    cors:  { origin: '*' }
-})
-
-setIO(io)
-
-notificationSocketHandler(io)
 
 connectDb()
 .then(() => {
-    server.listen(process.env.PORT || 5006, () => {
+    app.listen(process.env.PORT || 5006, () => {
         console.log(`Notification service is running on port ${process.env.PORT}`)
     })
 
